4914a34e7c4cd5868958d0a4a6f4c6678224de5b
AnishRane13 <anishrane178@gmail.com>
2024-06-14 19:20:48 +0530

    Basic Types in TypeScript

---
+++ Lesson-02/build/js/main.js
@@ -0,0 +1,13 @@
+"use strict";
+let myName = 'Daveeee';
+let meaningOfLife;
+let isLoading;
+let album;
+myName = 'John';
+meaningOfLife = 34;
+isLoading = true;
+album = 1828;
+const sum = (a, b) => {
+    return a + b;
+};
+let re = /\w+/g;
---
+++ Lesson-02/src/main.ts
@@ -0,0 +1,15 @@
+let myName: string = 'Daveeee'
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: string | number;
+
+myName = 'John'
+meaningOfLife = 34;
+isLoading = true;
+album = 1828
+
+const sum = (a: number, b: string) =>  {
+    return a + b;
+}
+
+let re: RegExp = /\w+/g
